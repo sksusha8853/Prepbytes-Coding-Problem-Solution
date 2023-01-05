fb46ad2d8fd853c48225791aa33297ced6be5067
Saurabh Kumar Singh <77530794+sksusha8853@users.noreply.github.com>
2023-01-05 10:25:23 +0530

    Update Leap_Year_Or_Not.cpp

--- Fundamentals/Leap_Year_Or_Not.cpp
+++ Fundamentals/Leap_Year_Or_Not.cpp
@@ -1,1 +1,34 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    if(n%4==0){
+      if(n%100==0){
+        if(n%400==0){
+          cout<<"Yes\n";
+        }
+        else{
+          cout<<"No\n";
+        }
+      }
+      else{
+        cout<<"Yes\n";
+      }
+      
+      
+    }
+    else{
+      cout<<"No\n";
+    }
+   
+  }
+  return 0;
+}
